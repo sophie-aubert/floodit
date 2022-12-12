56abeae1f270648733f4611595a5fb151b02ff8e
Simon Pinkas <212782+simonpinkas@users.noreply.github.com>
2022-12-12 09:37:04 +0100

    Change jvm build architecture

--- maven-install.sh
+++ maven-install.sh
@@ -13,7 +13,7 @@ sudo ln -s /opt/apache-maven-$TMP_MAVEN_VERSION /opt/maven
 # Setup environment variables
 sudo touch /etc/profile.d/maven.sh
 sudo chmod +x /etc/profile.d/maven.sh
->> /etc/profile.d/maven.sh echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64"
+>> /etc/profile.d/maven.sh echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64"
 >> /etc/profile.d/maven.sh echo "export M2_HOME=/opt/maven"
 >> /etc/profile.d/maven.sh echo "export MAVEN_HOME=/opt/maven"
 >> /etc/profile.d/maven.sh echo "export PATH=\$M2_HOME/bin:\$PATH"
